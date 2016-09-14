b7003aed1a1899ba7f42f47f5d6068fec46bba6c
bolddp <daniel.persson@kapsch.net>
2016-09-14 23:20:01 +0200

    Minor layout changes

--- Android/app/src/main/java/se/danielkonsult/www/kvadratab/activities/MainActivity.java
+++ Android/app/src/main/java/se/danielkonsult/www/kvadratab/activities/MainActivity.java
@@ -1,12 +1,21 @@
 package se.danielkonsult.www.kvadratab.activities;
 
 import android.graphics.Bitmap;
+import android.os.Handler;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import se.danielkonsult.www.kvadratab.AppCtrl;
 import se.danielkonsult.www.kvadratab.R;
@@ -18,13 +27,18 @@ public class MainActivity extends AppCompatActivity implements DataServiceListen
 
     // Private variables
 
-    private TextView _tvMain;
+    private final Handler _handler = new Handler();
+
+    private RelativeLayout _layoutConsultantImage;
+    private ImageView _imgConsultant;
+    private TextView _tvLoading;
+    private ProgressBar _progbarMain;
 
     // Private methods
 
-    private void addText(String text){
-        String existingText = _tvMain.getText().toString();
-        _tvMain.setText(existingText + text + "\r\n");
+    private void fadeInLoadingViews() {
+        _layoutConsultantImage.setVisibility(View.VISIBLE);
+        _tvLoading.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -32,29 +46,42 @@ public class MainActivity extends AppCompatActivity implements DataServiceListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        _tvMain = (TextView) findViewById(R.id.tvMain);
-        _tvMain.setMovementMethod(new ScrollingMovementMethod());
+        _layoutConsultantImage = (RelativeLayout) findViewById(R.id.layoutConsultantImage);
+        _imgConsultant = (ImageView) findViewById(R.id.imgConsultant);
+        _tvLoading = (TextView) findViewById(R.id.tvLoading);
 
-        // this.deleteDatabase(KvadratDb.DATABASE_NAME);
+        _progbarMain = (ProgressBar) findViewById(R.id.progbarMain);
+        _progbarMain.setProgress(0);
+
+        this.deleteDatabase(KvadratDb.DATABASE_NAME);
 
         AppCtrl.setApplicationContext(getApplicationContext());
 
         AppCtrl.getDataService().registerListener(this);
-        AppCtrl.getDataService().start();
+
+        // Start the data service with a short delay
+        _handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                AppCtrl.getDataService().start();
+            }
+        }, 3000);
+
     }
 
     @Override
     public void onInitialLoadStarted() {
-        addText("Initial load!");
+        fadeInLoadingViews();
     }
 
     @Override
     public void onInitialLoadProgress(int progressCount, int totalCount) {
+        _progbarMain.setProgress((int)((progressCount * 100.0) / totalCount));
     }
 
     @Override
     public void onConsultantAdded(ConsultantData consultant, Bitmap bitmap) {
-        addText(String.format("Added consultant %s (id %d)", consultant.Name, consultant.Id));
+        _imgConsultant.setImageBitmap(bitmap);
     }
 
     @Override
